8d7aad2cb07d752a4700fbcb953975aa5ae581d5
Don.V <Don.V@IN2377485W2.ey.net>
2020-06-26 17:47:14 +0530

    KubenateFiles

--- alpha/src/main/java/com/microservice/boot/alpha/models/User.java
+++ alpha/src/main/java/com/microservice/boot/alpha/models/User.java
@@ -1,12 +1,9 @@
 package com.microservice.boot.alpha.models;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+
 public class User {
 
-    @Id
     private String id;
     private String name;
     private String teamName;
